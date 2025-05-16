3bc3d75256346ff42e2d96362e918dfc8080c740
Gbolahan Akande <geakande@gmail.com>
2025-05-16 13:34:54 +0100

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2025 Co-working Space Booking System</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
