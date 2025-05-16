c89157149bdeaa9738ab4d04052532041c6c6577
Gbolahan Akande <geakande@gmail.com>
2025-05-16 14:01:21 +0100

    Updated the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import DeskList from './components/DeskList';
-import BookingForm from './components/BookingForm';
-import BookingSummary from './components/BookingSummary';
-import { BookingProvider } from './context/BookingContext';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import DeskList from './components/DeskList.jsx';
+import BookingForm from './components/BookingForm.jsx';
+import BookingSummary from './components/BookingSummary.jsx';
+import { BookingProvider } from './context/BookingContext.jsx';
 
 function App() {
   return (
